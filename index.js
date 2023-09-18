1f9e2719c630c629da328034628f831b01d94e76
Rishikeshghosh <rishikeshghoshghosh@gmail.com>
2023-09-18 14:15:29 +0530

    h

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config();
 app.use(express.json());
-app.use(helmet());
+/* app.use(helmet());
 //app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(
   helmet.contentSecurityPolicy({
@@ -34,7 +34,7 @@ app.use(
       "img-src": ["'self'", "https: data:"],
     },
   })
-);
+); */
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
